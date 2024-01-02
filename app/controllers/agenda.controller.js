2aefbf3753603558008bf3cd0d4ffb2fdb16a268
BlyatMan221 <119160448+HelloWorldYessir420@users.noreply.github.com>
2024-01-02 16:50:44 +0700

    tambahan agenda
    yes

--- app/controllers/agenda.controller.js
+++ app/controllers/agenda.controller.js
@@ -43,14 +43,14 @@ exports.create = async (req, res) => {
         const {name, start_date, end_date} = req.body;
         console.log("req body: ", req.body);
         if (!name || !start_date || !end_date) {
-            return res.status(400).json({ error: "name, start date, end date are required" });
+            return res.status(400).json({ error: "name, start time, and end time are required" });
         }
         if (isNaN(Date.parse(start_date)) || isNaN(Date.parse(end_date))) {
-            return res.status(400).json({ error: "start date and end date must be valid date" });
+            return res.status(400).json({ error: "start time and end time must be valid date" });
         }
 
         if (await checkOverlap(start_date, end_date)) {
-            return res.status(400).json({ error: "Agenda is overlapping with existing agenda" });
+            return res.status(400).json({ error: "Agenda is overlapping" });
         }
         const newAgenda = await Agenda.create({
             name,
@@ -63,25 +63,83 @@ exports.create = async (req, res) => {
     }
 }
 
+exports.findOne = async (req, res) => {
+    try {
+        const {id} = req.params;
+        const agenda = await Agenda.findOne({
+            where: {id: id}
+        });
+        if (!agenda) {
+            return res.status(404).json({ error: "Agenda empty" });
+        }
+        return res.status(200).json(agenda);
+    } catch (e) {
+        res.status(500).json({ error: e.message });
+    }
+}
+
+exports.findAll = async (req, res) => {
+    try {
+        let agenda;
+        const {month, year} = req.query;
+        if (month && year) {
+            agenda = await Agenda.findAll({
+                where: {
+                    [Op.or]: [
+                        {
+                            start_date: {
+                                [Op.and]: [
+                                    {[Op.gte]: new Date(year, month-1, 1)},
+                                    {[Op.lte]: new Date(year, month, 0)}
+                                ]
+                            }
+                        },
+                        {
+                            end_date: {
+                                [Op.and]: [
+                                    {[Op.gte]: new Date(year, month-1, 1)},
+                                    {[Op.lte]: new Date(year, month, 0)}
+                                ]
+                            }
+                        },
+                        {
+                            [Op.and]: [
+                                {start_date: {[Op.lte]: new Date(`${year}-${month}-01`)}},
+                                {end_date: {[Op.gte]: new Date(`${year}-${month}-01`)}}
+                            ]
+                        }
+                    ]
+                }
+            });
+        } else {
+            agenda = await Agenda.findAll();
+        }
+        
+        return res.status(200).json(agenda);
+    } catch (e) {
+        res.status(500).json({ error: e.message });
+    }
+}
+
 
 exports.update = async (req, res) => {
     try {
         const {id} = req.params;
         const {name, start_date, end_date} = req.body;
         if (!name || !start_date || !end_date) {
-            return res.status(400).json({ error: "name, start date, end date are required" });
+            return res.status(400).json({ error: "name, start time, and end time are required" });
         }
         if (isNaN(Date.parse(start_date)) || isNaN(Date.parse(end_date))) {
-            return res.status(400).json({ error: "start date and end date must be valid date" });
+            return res.status(400).json({ error: "start time and end time must be valid date" });
         }
         const agenda = await Agenda.findOne({
             where: {id: id}
         });
         if (await checkOverlap(start_date, end_date, id)) {
-            return res.status(400).json({ error: "Agenda is overlapping with existing agenda" });
+            return res.status(400).json({ error: "Agenda is overlapping" });
         }
         if (!agenda) {
-            return res.status(404).json({ error: "Agenda not found" });
+            return res.status(404).json({ error: "Agenda empty" });
         }
         agenda.name = name;
         agenda.start_date = new Date(start_date);
@@ -100,10 +158,10 @@ exports.delete = async (req, res) => {
             where: {id: id}
         });
         if (!agenda) {
-            return res.status(404).json({ error: "Agenda not found" });
+            return res.status(404).json({ error: "Agenda empty" });
         }
         await roomSchedule.destroy();
-        return res.status(200).json({message: "Agenda deleted successfully"});
+        return res.status(200).json({message: "Agenda cleared"});
     } catch (e) {
         res.status(500).json({ error: e.message });
     }
--- app/routes/agenda.routes.js
+++ app/routes/agenda.routes.js
@@ -3,7 +3,9 @@ const router = require('express').Router();
 const authMiddleware = require('../Middleware/auth.middleware')
 
 router.post('/', authMiddleware.authenticateToken, agendaController.create);
+router.get('/', authMiddleware.authenticateToken, agendaController.findAll);
 router.put('/:id', authMiddleware.authenticateToken, agendaController.update);
 router.delete('/:id', authMiddleware.authenticateToken, agendaController.delete);
+router.get('/:id', authMiddleware.authenticateToken, agendaController.findOne);
 
 module.exports = router;
